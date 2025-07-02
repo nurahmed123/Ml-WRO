686ebbc0f6a3d807e08a2b7e1ac5709834cf7929
raghib112 <06nurahmed@gmail.com>
2025-07-02 12:21:37 +0600

    test pi cam

--- camera.py
+++ camera.py
@@ -1,8 +1,17 @@
+import cv2
 from picamera2 import Picamera2
-import time
+import numpy as np
 
 picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888"}))
 picam2.start()
-time.sleep(2)
-picam2.capture_file("image.jpg")
-print("Image saved")
+
+while True:
+    frame = picam2.capture_array()
+    cv2.imshow("Picamera2 preview", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+picam2.close()
+cv2.destroyAllWindows()
